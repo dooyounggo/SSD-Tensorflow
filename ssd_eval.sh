af6730da58158180ad6dd4fabe6c843126935550
dooyounggo <dooyounggo@gmail.com>
2020-06-30 11:30:08 +0900

    Update ssd_eval.sh

--- ssd_eval.sh
+++ ssd_eval.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-echo "Enter directory name:"
-read input
+echo "Enter subdirectory name:"
+read -r input
 
 DATASET_DIR="./tfrecords/${input}"
 EVAL_DIR="./logs/${input}"
 CHECKPOINT_PATH="./checkpoints/VGG_VOC0712_SSD_300x300_ft_iter_120000.ckpt"
 
 echo "Dataset: ${DATASET_DIR}, log: ${EVAL_DIR}, ckpt: ${CHECKPOINT_PATH}"
-mkdir -p ${EVAL_DIR}
-rm -rf ${EVAL_DIR}/events*
+mkdir -p "${EVAL_DIR}"
+rm -rf "${EVAL_DIR}/events*"
 
 python eval_ssd_network.py \
-    --eval_dir=${EVAL_DIR} \
-    --dataset_dir=${DATASET_DIR} \
+    --eval_dir="${EVAL_DIR}" \
+    --dataset_dir="${DATASET_DIR}" \
     --dataset_name=pascalvoc_2007 \
     --dataset_split_name=test \
     --model_name=ssd_300_vgg \
-    --checkpoint_path=${CHECKPOINT_PATH} \
-    ${@}
-
+    --checkpoint_path="${CHECKPOINT_PATH}" \
+    "${@}"
